2d7e76fec20c8a59439727eb226e539ddf2c2c9c
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-09-18 21:26:43 -0300

    Formulário de comentários

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -42,6 +42,13 @@ export const Post = (props: PostProps) => (
           <a href='#'>#rocketseat</a>
         </p>
       </div>
+      <form className={styles.commentForm}>
+        <strong>Deixe o seu feedback</strong>
+        <textarea placeholder='Deixe um comentário'/>
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+      </form>
     </article>
   </>
 );
